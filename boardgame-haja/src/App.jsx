8dd5683540f8db3e331a99da15dcd4aacdbff043
ho-ji <wkdd0205@gmail.com>
2023-11-02 15:49:08 +0900

    design: reset css 설정

--- boardgame-haja/src/App.jsx
+++ boardgame-haja/src/App.jsx
@@ -1,10 +1,45 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {createGlobalStyle} from 'styled-components'
+import reset from 'styled-reset'
 
 import Home from 'pages/Home'
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  *{
+    box-sizing: border-box;
+  }
+  button{
+    border: none;
+    cursor: pointer;
+    padding: 0;
+  }
+  input{
+    padding: 0;
+    &:hover{
+      outline:none;
+    }
+    &:focus{
+      outline:none;
+    }
+  }
+  html{
+    font-size: 62.5%;
+  }
+  body{
+    font-size: 1.6rem;
+  }
+  a{
+    text-decoration: none;
+    font-size: inherit;
+    color: inherit;
+  }
+`
 
 const App = () => {
   return (
     <BrowserRouter>
+      <GlobalStyle />
       <Routes>
         <Route
           path="/"
