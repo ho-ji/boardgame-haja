632ab10837ea4e1db0c6dd858a8f374e84b6cd18
ho-ji <wkdd0205@gmail.com>
2023-11-05 22:46:51 +0900

    design: 웹접근성을 위한 대체 텍스트 스타일 추가

--- boardgame-haja/src/App.jsx
+++ boardgame-haja/src/App.jsx
@@ -37,6 +37,16 @@ const GlobalStyle = createGlobalStyle`
     font-size: inherit;
     color: inherit;
   }
+  .a11y-hidden{
+    clip: rect(1px, 1px, 1px, 1px);
+    clip-path: inset(50%);
+    width: 1px;
+    height: 1px;
+    margin: -1px;
+    overflow: hidden;
+    padding: 0;
+    position: absolute;
+  }
 `
 
 const App = () => {
