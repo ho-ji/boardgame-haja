98a11d7e9656952476d05c9f396efd2a10b70af4
ho-ji <wkdd0205@gmail.com>
2023-11-09 23:53:57 +0900

    chore: import 경로 수정

--- boardgame-haja/src/pages/Detail.jsx
+++ boardgame-haja/src/pages/Detail.jsx
@@ -1,6 +1,6 @@
 import Footer from 'components/common/Footer'
 import Header from 'components/common/Header'
-import DetailInfo from '../components/detail/DetailInfo'
+import DetailInfo from 'components/detail/DetailInfo'
 
 const Detail = () => {
   return (
