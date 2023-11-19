7ec5e724d915aec9846c4cbe55efc71e0f41fe98
ho-ji <wkdd0205@gmail.com>
2023-11-20 00:03:45 +0900

    feat: 카카오 지도 API 사용하여 현재 위치 지도 로딩

--- boardgame-haja/src/components/location/LocationBoardGameCafe.jsx
+++ boardgame-haja/src/components/location/LocationBoardGameCafe.jsx
@@ -1,14 +1,46 @@
+import {useEffect, useRef, useState} from 'react'
 import styled from 'styled-components'
+
+import useCurrentLocation from 'hooks/useCurrentLocation'
+
+const {kakao} = window
 
 const Container = styled.main`
   display: flex;
   align-items: center;
   justify-content: center;
-  height: calc(100vh - var(--header-height) - var(--footer-height));
+  margin: 10rem 0;
+`
+const Map = styled.div`
+  width: 80%;
+  height: 30rem;
 `
 
 const LocationBoardGameCafe = () => {
-  return <Container></Container>
+  const mapRef = useRef(null)
+  const [map, setMap] = useState()
+  const {location: currentLocation} = useCurrentLocation()
+
+  useEffect(() => {
+    const options = {
+      center: new kakao.maps.LatLng(37.553881, 126.970488),
+      level: 6,
+    }
+    setMap(new kakao.maps.Map(mapRef.current, options))
+  }, [currentLocation])
+
+  useEffect(() => {
+    if (currentLocation && map) {
+      const currentPosition = new kakao.maps.LatLng(currentLocation.latitude, currentLocation.longitude)
+      map.panTo(currentPosition)
+    }
+  }, [currentLocation, map])
+
+  return (
+    <Container>
+      <Map ref={mapRef}></Map>
+    </Container>
+  )
 }
 
 export default LocationBoardGameCafe
