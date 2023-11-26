3c6ebb6b4c84174968fbad0b7b3287ce0e8fe49f
ho-ji <wkdd0205@gmail.com>
2023-11-26 16:59:52 +0900

    design: not found 페이지 반응형 추가

--- boardgame-haja/src/styles/notFoundStyle.js
+++ boardgame-haja/src/styles/notFoundStyle.js
@@ -1,26 +1,34 @@
 import styled from 'styled-components'
+import {BREAKPOINT_MOBILE, mediaQueris} from './mediaQuery'
 
 export const Container = styled.main`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  text-align: center;
   height: calc(100vh - var(--header-height));
 `
 export const Image = styled.img`
   width: 10rem;
   margin-bottom: 1rem;
+  ${mediaQueris(BREAKPOINT_MOBILE)} {
+    width: 5rem;
+  }
 `
 export const Title = styled.h2`
   font-size: 4rem;
   font-weight: bold;
+  ${mediaQueris(BREAKPOINT_MOBILE)} {
+    font-size: 2rem;
+  }
 `
 
 export const HomeButton = styled.button`
   margin-top: 2rem;
   font-weight: bold;
   color: black;
-  padding: 1% 2rem;
+  padding: 1rem 2rem;
   border-radius: 4px;
   background: black;
   color: white;
@@ -29,5 +37,8 @@ export const HomeButton = styled.button`
   &:hover {
     color: black;
     background: white;
+  }
+  ${mediaQueris(BREAKPOINT_MOBILE)} {
+    padding: 0.5rem 1rem;
   }
 `
